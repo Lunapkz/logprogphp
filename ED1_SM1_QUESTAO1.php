a866f8ede2c9933f7568668f3cc6b4a62dbe7296
Iury Luciano de O. Luna <89174551+Lunapkz@users.noreply.github.com>
2022-03-07 06:23:35 -0300

    ED1_SM1_QUESTAO1

---
+++ ED1_SM1_QUESTAO1.php
@@ -0,0 +1,20 @@
+<?php
+//input
+$valor1 = 10;
+$valor2 = 100;
+$valor3 = 1000;
+
+//processamento
+
+$soma= $valor1 + $valor2 + $valor3;
+
+// output
+
+echo "<H1> SEMANA 1 </H1>";
+echo "<FONT color=black>";
+echo "<P> 1- Faça um algoritmo em PHP no caderno ou no editor de texto (VSCode / Notepad ) que receba três números inteiros e calcule a soma deles.<P>";
+print " <BR> A soma dos valores é : ".$soma;
+echo "</FONT>";
+echo " <BR> <BR> FIM";
+
+?>
